76cd7f360ae043fcd328a8e49b4427b64cbe33f7
Rashidprojects <rashidgitproject@gmail.com>
2024-07-11 19:58:14 +0530

    * nabar section and logo setted, * Framer motion animation added for hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import BgImage from "../../assets/coffee-bg.jpg"
 import BlackCoffee from "../../assets/coffee-cup.png"
+import Navbar from '../Navbar/Navbar'
+import { motion } from "framer-motion"
 
 const bgImage = {
   backgroundImage: `url(${BgImage})`,
@@ -15,12 +17,31 @@ const Hero = () => {
         <section className='min-h-[750px] w-full'>
             <div className="container">
                 {/* Navbar Section */}
+                <Navbar />
                 {/* Hero Section */}
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]'>
                   {/* text content section */}
                   <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
-                    <h1 className=' text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
-                    <div className='relative'>
+                    <motion.h1 
+                      initial={{ opacity:0, y:-100 }}
+                      animate={{ opacity:1, y:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:1,
+                      }}
+                      className=' text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</motion.h1>
+                    <motion.div 
+                      initial={{ opacity:0, y:-100 }}
+                      animate={{ opacity:1, y:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:1.2,
+                      }}
+                      className='relative'>
                       <div className='relative z-10 space-y-4'>
                         <h1 className='text-2xl'>Black Lifesttyle Lovers</h1>
                         <h1 className='text-sm opacity-55 leading-loose'>
@@ -28,25 +49,62 @@ const Hero = () => {
                         </h1>
                       </div>
                       <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
-                    </div>
+                    </motion.div>
                   </div>
                   {/* hero image section */}
                   <div className='relative'>
-                    <img src={BlackCoffee} alt="" 
-                      className='relative z-40 h-[400px] md:h-[700px] img-shadow'
+                    <motion.img 
+                      initial={{ opacity:0, scale:0 }}
+                      animate={{ opacity:1, scale:1 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:0.4,
+                      }}
+                      src={BlackCoffee} alt="" 
+                      className='relative mt-10 z-40 h-[400px] md:h-[700px] img-shadow'
                     />
 
                     {/* orange circle ring */}
-                    <div className='h-[180px] w-[180px] absolute top-20 -right-2 border-primary border-[20px] rounded-full z-10'></div>
+                    <motion.div 
+                      initial={{ opacity:0, y:100 }}
+                      animate={{ opacity:1, y:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:0.8,
+                      }}
+                      className='h-[180px] w-[180px] absolute top-16 right-1 border-primary border-[20px] rounded-full z-10'>
+                    </motion.div>
 
                     {/* big text section */}
-                    <div className='absolute -top-20 left-[200px] z-[1]'>
-                      <h1 className='text-[140px] scale-150 font-bold text-darkGray/40 leading-none'>Blvck Tumbler</h1>
-                    </div>
+                    <motion.div 
+                      initial={{ opacity:0, x:-100 }}
+                      animate={{ opacity:1, x:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:0.8,
+                      }}
+                      className='absolute -top-20 left-[200px] z-[1]'>
+                      <h1 className='text-[140px] scale-150 font-bold text-darkGray/40 leading-none overflow-hidden'>Blvck Tumbler</h1>
+                    </motion.div>
                   </div>
 
                   {/* third div section */}
-                  <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+                  <motion.div 
+                    initial={{ opacity:0, y:-100 }}
+                      animate={{ opacity:1, y:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:1.2,
+                      }}
+                    className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
                     <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
                     <div className='relative'>
                       <div className='relative z-10 space-y-4'>
@@ -58,7 +116,7 @@ const Hero = () => {
                       </div>
                       <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50'></div>
                     </div>
-                  </div>
+                  </motion.div>
 
                 </div>
             </div>
