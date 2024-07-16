8893c36cff6065ec8ce774050f56d7d775b2c3da
Rashidprojects <rashidgitproject@gmail.com>
2024-07-16 11:11:11 +0530

    Coffe services section completed with animation of screen view

---
+++ src/components/Services/Data.jsx
@@ -0,0 +1,26 @@
+import coffee1 from '../../assets/product-1.png'
+import coffee2 from '../../assets/product-2.png'
+import coffee3 from '../../assets/product-3.png'
+
+const Data = [
+    {
+        id : 1,
+        image : coffee1,
+        title : "Cold Coffee",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorem eius eligendi rem consequuntur excepturi inventore necessitatibus ut iure deserunt" 
+    },
+    {
+        id : 2,
+        image : coffee3,
+        title : "Black Coffee",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorem eius eligendi rem consequuntur excepturi inventore necessitatibus ut iure deserunt" 
+    },
+    {
+        id : 3,
+        image : coffee2,
+        title : "Hot Coffee",
+        subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorem eius eligendi rem consequuntur excepturi inventore necessitatibus ut iure deserunt" 
+    }
+]
+
+export default Data
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Data from './Data'
+
+const cardVariants = {
+  hidden : { opacity: 0, y:20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 150,
+      damping: 10,
+      ease: "easeInOut"
+    },
+  },
+}
+
+const containerVariants = {
+  hidden: { opacity:1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.6,
+      staggerChildren:0.4,
+    }
+  }
+}
+
+const Services = () => {
+  return (
+    <div className='container my-16 space-y-4'>
+      {/* header section */}
+
+      <div className='text-center max-w-lg mx-auto space-y-2'>
+        <motion.h1 
+          initial={{ opacity:0, y:-100 }}
+                      animate={{ opacity:1, y:0 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:0.2,
+                      }}
+          className='text-3xl font-bold text-lightGray'>
+          Fresh and <span className='text-primary'>Tasty coffee</span>
+        </motion.h1>
+
+        <motion.p 
+          initial={{ opacity:0, scale:0.5 }}
+                      animate={{ opacity:1, scale:1 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping:10,
+                        delay:0.6,
+                      }}
+          className='text-sm opacity-50'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, deleniti esse Voluptates cupiditate doloremque rem
+        </motion.p>
+      </div>
+
+      {/* card section */}
+      <motion.div 
+        variants={containerVariants}
+        initial="hidden"
+        whileInView={"visible"}
+        viewport={{ amount: 0.8 }}
+        className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
+
+        {Data.map((service) => (
+          <motion.div 
+            variants={cardVariants}
+            className='text-center p-4 flex flex-col items-center space-y-6' >
+            <img src={service.image} alt="" />
+            <h1>{service.title}</h1>
+            <p>{service.subtitle}</p>
+          </motion.div>
+        ))}
+
+      </motion.div>
+    </div>
+  )
+}
+
+export default Services
