5adf5197163090cd6cb040c44a96a2cec311d96a
Rashidprojects <rashidgitproject@gmail.com>
2024-07-17 14:22:33 +0530

    AppBanner section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AppBanner from "./components/AppBanner/AppBanner"
 import Hero from "./components/Hero/Hero"
 import Services from "./components/Services/Services"
 import WhereToBuy from "./components/WhereToBuy/WhereToBuy"
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Services />
       <WhereToBuy />
+      <AppBanner />
     </>
   )
 }
